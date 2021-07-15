983b708f9044fa8a6603110824f53babe3d3829b
lamtanphiho <lamtanphiho1@gmail.com>
2021-07-16 01:05:53 +0700

    updatte for mainnet lottery

--- manager/lottery.js
+++ manager/lottery.js
@@ -84,11 +84,11 @@ const handler = async () => {
     const currentLotteryId = await contract.methods.viewCurrentLotteryId().call()
     const [lottery] = await Promise.all([contract.methods.viewLottery(currentLotteryId).call()])
     // console.log(currentLotteryId, lottery)
-    if(lottery.status == 3) startLottery()
+    if(currentLotteryId == 0 || lottery.status == 3) startLottery()
     if(lottery.status == 1 && Number(lottery.endTime) < (moment().unix())) closeLottery(currentLotteryId)
     if(lottery.status == 2) drawFinalNumberAndMakeLotteryClaimable(currentLotteryId, true)
 }
-const cronjob = new CronJob('*/30 * * * * *', () => {
+const cronjob = new CronJob('1 */1 * * * *', () => {
     handler()
 })
 cronjob.start()
